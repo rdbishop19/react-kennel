8424bc48ebf4007efa07be9c0206c5a1767b8d3a
Ryan Bishop <rdbishop06@gmail.com>
2019-11-18 13:07:53 -0600

    nav (routing) + application view

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './NavBar.css'
+
+class NavBar extends Component {
+    render(){
+        return (
+            <header>
+                <h1 className="site-titel">Student Kennels
+                    <small>Loving care</small>
+                </h1>
+                <nav>
+                    <ul className="container">
+                        <li><Link className="nav-link" to="/">Home</Link></li>
+                        <li><Link className="nav-link" to="/animals">Animals</Link></li>
+                        <li><Link className="nav-link" to="/locations">Locations</Link></li>
+                        <li><Link className="nav-link" to="/employees">Employees</Link></li>
+                        <li><Link className="nav-link" to="/owners">Owners</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default NavBar;
---
+++ src/components/owner/OwnerCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class OwnerCard extends Component {
+	render() {
+		return (
+			<div>
+				<h3>
+					Owner Name: <span className="card-ownername">Jim</span>
+				</h3>
+			</div>
+		);
+	}
+}
+
+export default OwnerCard;
